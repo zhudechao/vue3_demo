942557dd2c9895da8c5a80e83eadd7171b311a43
zhudechao <1474083407@qq.com>
2021-03-30 15:37:26 +0800

    跑马灯

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,56 @@
+<style>
+    html,body{
+        padding: 0;
+        margin: 0;
+    }
+    .el-main{
+        padding: 0;
+    }
+    .el-header, .el-footer {
+        background-color: #B3C0D1;
+        color: #333;
+        text-align: center;
+        line-height: 60px;
+    }
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 14px;
+        opacity: 0.75;
+        line-height: 200px;
+        margin: 0;
+    }
+
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
+    }
+</style>
+
 <template>
     <el-container>
         <el-header>Header</el-header>
         <el-main>
-            <el-button type="primary">主要按钮</el-button>
+            <el-carousel  type="card">
+                <el-carousel-item v-for="item in items" :key="item">
+                    <el-image
+                            style="width: 100%;"
+                            :src="item.url"
+                            ></el-image>
+                </el-carousel-item>
+            </el-carousel>
         </el-main>
     </el-container>
 </template>
 
-<style>
-html,body{
-    padding: 0;
-    margin: 0;
-}
-.el-header, .el-footer {
-    background-color: #B3C0D1;
-    color: #333;
-    text-align: center;
-    line-height: 60px;
-}
-
-.el-aside {
-    background-color: #D3DCE6;
-    color: #333;
-    text-align: center;
-    line-height: 200px;
-}
-
-.el-main {
-    background-color: #E9EEF3;
-    color: #333;
-    text-align: center;
-    line-height: 160px;
-}
-
-body > .el-container {
-    margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-    line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-    line-height: 320px;
-}
-</style>
+<script>
+    export default {
+        data:function () {
+            return {
+                items:[{url:"/src/assets/1.jpg"},{url:"/src/assets/1.jpg"},{url:"/src/assets/1.jpg"},{url:"/src/assets/1.jpg"}]
+            }
+        }
+    }
+</script>
